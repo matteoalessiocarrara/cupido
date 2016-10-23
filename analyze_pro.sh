1d88580223399b0f9573f31a42b2b1ac87185ede
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-10-23 17:05:12 +0200

    Fixato l ordinamento dei mi piace comuni, e aggiunto output in csv

--- analyze_pro.sh
+++ analyze_pro.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Copyright 2016 Matteo Alessio Carrara <sw.matteoac@gmail.com>
 
-cat $@ | sort | uniq -c | sort -r
+python <<< "
+for line in '''$(cat $@ | sort | uniq -c | sort -n -r)'''.split('\n'):
+	l = line.strip()
+	num = l.split(' ')[0]
+	t = l[len(num) + 1:]
+	print ('\"%s\", %s' % (t, num))
+"
+
+
 
