45e5fed21ebbdb314dfe1eb810dd4151e455d136
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-10-24 13:54:01 +0200

    Sostanziali miglioramenti allo script per scaricare i like

--- download_likes.py
+++ download_likes.py
@@ -4,7 +4,9 @@
 
 
 # Questo script può essere utilizzato per scaricare i 'mi piace' di varie persone 
-# in una directory, per poi crearci un modello.
+# in una directory, per poi crearci un modello o semplicemente analizzarle successivamente.
+# È necessario scaricare più persone in una volta sola poiché facendo troppe volte
+# il login su facebook si rischia di essere bloccati.
 
 
 from sys import argv
@@ -14,37 +16,41 @@ from fbwrapper import fbwrapper
 
 
 
-def write_likes(likes, fpath):
-	if len(likes) == 0:
-		logging.warning("Il file '%s' non è stato scritto poiché vuoto" % fpath)
-		return
+class LikesDownloader:
 
-	f = open(fpath, "w")
+	def __init__(self, username, password, download_dir, profiles, fb_obj=None):
+		"""Se fb_obj è diverso da None, verrà usato quello invece di fare un nuovo login"""
+		self.__fb = fb_obj if fb_obj != None else fbwrapper.Facebook(username, password)
+		self.__dir = download_dir
+		self.__profiles = profiles
+		
+		self.__downloadLikes()
 	
-	for like in likes:
-		f.write(like + "\n")
 	
-	f.close()
+	def __downloadLikes(self):
+		for profile in self.__profiles:
+			self.__write_likes(self.__fb.get_profile(profile).get_likes(), "%s/%s" % (self.__dir,  profile))
+		
+
+	def __write_likes(self, likes, fpath):
+		"""Scrive i 'mi piace' di un profilo in un file"""
+		if len(likes) == 0:
+			logging.warning("Il file '%s' non è stato scritto poiché vuoto" % fpath)
+			return
+
+		with open(fpath, "w") as f:	
+			for like in likes:
+				f.write(like + "\n")
 
 
+if __name__ == "__main__":
+	try:
+		username, password, download_dir = argv[1:4]
+		usernames = argv[4:]
+	except ValueError:
+		exit("Uso: download_likes.py username password download_dir url1...urlN")
 
-try:
-	username, password = argv[1:3]
-except IndexError:
-	exit("Uso: download_likes.py username password")	
-
-
-logging.getLogger().setLevel(logging.INFO)
-fb = fbwrapper.Facebook(username, password)
-
-# Modificare, directory dove salvare i mi piace
-PATHPR="/home/richard/Documents/pro/"
-
-
-# Inserire qui le istruzioni per scaricare i mi piace
-# Es
-# write_likes(fb.get_profile("LINK").get_likes(), PATHPR + "nomefile")
-
-print("Questo script deve essere modificato per funzionare, leggere il sorgente")
+	logging.getLogger().setLevel(logging.INFO)
+	LikesDownloader(username, password, download_dir, usernames)
 
 	
