e91216732ba72824eafcf620b3a6fe6ed1abbae6
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-10-24 20:29:22 +0200

    Aggiunto script per rimuovere like duplicati dai profili

---
+++ sanitize_likes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2016 Matteo Alessio Carrara <sw.matteoac@gmail.com>
+
+# Rimuove i like duplicati in ogni profilo
+# Input: la directory con i mi piace
+
+
+if [[ $# -ne 1 ]]; then
+	echo "Passare la directory con i mi piace"
+	exit 1
+fi
+
+TMPDIR=/tmp/$(echo $1 | md5sum | cut -d ' ' -f1)
+mkdir $TMPDIR
+
+for file in $(ls $1)
+do
+	cat "$1/$file" | sort | uniq -u > "$TMPDIR/$file"
+	mv "$TMPDIR/$file" "$1/$file"
+done
+
+rm -rf $TMPDIR
