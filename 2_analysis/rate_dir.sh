7ea61ed217fe0b6f1a7a1a4a65f8c5e1906154f0
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-11-07 18:25:53 +0100

    Aggiornato wrapper per rate.py

--- 2_analysis/rate_dir.sh
+++ 2_analysis/rate_dir.sh
@@ -2,15 +2,14 @@
 # Copyright 2016 Matteo Alessio Carrara <sw.matteoac@gmail.com>
 
 
-if [[ $# -ne 3 ]]; then
-	echo "Uso: directory_confrontare modello_csv membri_csv"
+if [[ $# -ne 2 ]]; then
+	echo "Uso: directory_confrontare modello_csv"
 	exit 1
 fi
 
 PR_DIR=$1
 CSV_MODEL=$2
-CSV_MEMBERS=$3
 
 for p in $(ls $PR_DIR); do
-	python rate.py $PR_DIR/$p $CSV_MODEL $CSV_MEMBERS
+	python rate.py $PR_DIR/$p $CSV_MODEL
 done
