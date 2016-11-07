f86cb4078f77e3a690bed1bd0b26d715d96cbf62
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-11-08 00:16:58 +0100

    Migliorato rating

--- 2_analysis/rate.py
+++ 2_analysis/rate.py
@@ -8,6 +8,10 @@ import csv
 import logging
 import math
 
+
+def x(a, b, max_dec):
+	return (100. - max_dec + (float(a) / b * max_dec )) / 100
+
 try:
 	profile_likes_f, model_likes_f = argv[1:3]
 except ValueError:
@@ -27,11 +31,16 @@ matching = 0
 goodness = 0
 with open(profile_likes_f) as pl:
 	for like in [l.strip('\n') for l in pl.readlines()]:
+		
 		if like in model_likes_names:
 			matching += 1
 			goodness += model_likes_value[model_likes_names.index(like)]
 	
 	pl.seek(0, 0)
-	rating = float(matching) / len(pl.readlines()) * float(goodness) / matching
+	likes = len(pl.readlines())
+	try:
+		rating = x(float(matching), likes, 30) * x(float(goodness), matching, 90) * x(likes, 500, 99)
+	except ZeroDivisionError:
+		rating = 0
 	print (rating, profile_likes_f)
 		
