7735b662813fa338905b32fdc1d2c1f58d0f3e76
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2017-02-03 18:14:59 +0100

    Added todo, fixed csv loading, implemented overloading of __sub__ method

--- 1_processing/libmodel.py
+++ 1_processing/libmodel.py
@@ -5,6 +5,9 @@
 import csv
 
 
+# TODO Anti-model, people weight
+
+
 class Model(object):
 	def __init__(self, csv=None, files=None):
 		"""The model can be loaded from a previously created csv file, or created 
@@ -17,9 +20,10 @@ class Model(object):
 			raise Exception("Uno dei parametri deve essere definito")
 
 
-	def __load_from_csv(self, csv):
-		with open(csv) as csvfile:
-			self.__set_likes(csv.DictReader(csvfile))
+	def __load_from_csv(self, csvf):
+		with open(csvf) as csvfile:
+			reader = csv.reader(csvfile)
+			self.__set_likes({rows[1]:float(rows[0]) for rows in reader})
 
 	
 	def __load_from_files(self, files):
@@ -41,3 +45,18 @@ class Model(object):
 	
 	def get_likes(self):
 		return self.__likes
+
+	
+	def __sub__(self, other):
+		"""other MUST BE an istance of this class"""
+		tl = self.get_likes()
+		ol = other.get_likes()
+		rl = {}
+		for key in set(list(tl.keys()) + list(ol.keys())):
+				v = 0 + tl[key] if key in tl.keys() else 0
+				v -= ol[key] if key in ol.keys() else 0
+				rl[key] = v
+				
+		return rl
+
+		
